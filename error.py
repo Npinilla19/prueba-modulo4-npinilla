74303c92e46c983017ee86185417a00351f30a16
Vanessa Morales N <vane.moralesn@gmail.com>
2024-09-09 22:39:00 +0000

    se incorporan docstrings archivo error.py

--- error.py
+++ error.py
@@ -1,6 +1,12 @@
 class SubTipoInvalidoException(Exception):
+    """
+    Excepción lanzada cuando el subtipo de un anuncio no es válido.
+    """
     pass
 
 
 class LargoExcedidoException(Exception):
+    """
+    Excepción lanzada cuando el nombre de la campaña excede los 250 caracteres.
+    """
     pass
